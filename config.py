fcaf156e39dfa8b6bb0523290e3897a4b54229c8
ShuRaw <rawat10shubham@gmail.com>
2020-09-29 13:49:34 +0530

    Removing pre-existing config

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ VIM_HOME = f'{HOME}/.vim'
 for filepath in glob.iglob(f'{HOME}/dotfiles/config/.*'):
     filename = filepath.split('/')[-1]
     try:
+        os.remove(f'{HOME}/{filename}')
         os.symlink(filepath, f'{HOME}/{filename}')
     except Exception as exception:
         print(exception)
