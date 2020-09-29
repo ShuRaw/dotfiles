0bb7992bbbab3d4dda0469ec1db284ac576eb2a7
ShuRaw <rawat10shubham@gmail.com>
2020-09-29 15:16:14 +0530

    Fixing file removal

--- config.py
+++ config.py
@@ -8,8 +8,10 @@ VIM_HOME = f'{HOME}/.vim'
 for filepath in glob.iglob(f'{HOME}/dotfiles/config/.*'):
     filename = filepath.split('/')[-1]
     try:
-        os.remove(f'{HOME}/{filename}')
-        os.symlink(filepath, f'{HOME}/{filename}')
+        dotfile = f'{HOME}/{filename}'
+        if os.path.exists(dotfile):
+            os.remove(dotfile)
+        os.symlink(filepath, dotfile)
     except Exception as exception:
         print(exception)
 
